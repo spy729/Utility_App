30b2211209259549d6b6b0a457f5334344cc813f
spy729 <rishiagarwal729@gmail.com>
2024-12-25 23:06:29 +0530

    fix

--- Utility_App/src/App.jsx
+++ Utility_App/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
+import AppRoutes from "./Routes/AppRoutes";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
